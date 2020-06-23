faf7f98518e40952b7f3a35588865cd4226511e2
Tejas Choudhary <tejasc256@gmail.com>
2020-06-23 18:19:42 +0530

    Added Files - II

--- MATLAB code/generate_dataset_4096.m
+++ MATLAB code/generate_dataset_4096.m
@@ -1,11 +1,11 @@
-function [Xtrain, Ytrain] = generate_dataset_1024(min_r,max_r,sigma)
-    Ytrain = zeros(10000,10);
+function [Xtrain, Ytrain] = generate_dataset_4096(min_r,max_r,sigma)
+    Ytrain = zeros(10000,12);
     rpart = min_r + (max_r-min_r).*rand(10000,1);
     ipart = min_r + (max_r-min_r).*rand(10000,1);
     Xtrain = horzcat(rpart,ipart);
     for j=1:10000
         c_no = rpart(j) + ipart(j)*i;
-        xllr = llr_1024(c_no,sigma);
+        xllr = llr_4096(c_no,sigma);
         Ytrain(j,1) = xllr(1,1);
         Ytrain(j,2) = xllr(1,2);
         Ytrain(j,3) = xllr(1,3);
@@ -16,5 +16,7 @@ function [Xtrain, Ytrain] = generate_dataset_1024(min_r,max_r,sigma)
         Ytrain(j,8) = xllr(1,8);
         Ytrain(j,9) = xllr(1,9);
         Ytrain(j,10) = xllr(1,10);
+        Ytrain(j,11) = xllr(1,11);
+        Ytrain(j,12) = xllr(1,12);
     end
 end
